3be27ca95ba9818de2ecd647278d537b410e9cf9
mrcjbradley <cjbradley@me.com>
2019-08-13 08:41:33 -0400

    css-lecture setup

--- src/components/taskList.jsx
+++
@@ -1,6 +0,0 @@
-import React, { useState } from "react";
-
-function TaskList(props) {
-  return null;
-}
-export default TaskList;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Layout from "./components/Layout";
 import "./styles/app.scss";
 
 if (process.env.NODE_ENV !== "production") {
   console.log("Development Mode Ahoy!");
 }
-
-ReactDOM.render(<Layout />, document.getElementById("app"));
